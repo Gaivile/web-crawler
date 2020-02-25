a40a60e9c5f19208a6e03a38b58be53c54386019
Gaivile <gaivile.vezeviciute@healthunlocked.com>
2020-02-25 10:31:33 +0000

    :fire: Initial commit

---
+++ src/web_crawler/core.clj
@@ -0,0 +1,9 @@
+(ns web-crawler.core
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.data.csv :as csv]))
+
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
